40f781e95561b2d72348f5b3998ed8a7abbfe2b2
Rafael Alexandre <mrrafaeldeoliveira@gmail.com>
2026-05-05 21:12:14 -0300

    fix(frontend): current month pina fixed

--- frontend/src/stores/currentMonth.ts
+++ frontend/src/stores/currentMonth.ts
@@ -2,7 +2,7 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useMonthStore = defineStore('month', () => {
-  const month = ref(1)
+  const month = ref(new Date().getMonth() + 1)
   function setMonth(m:number) {
     month.value = m;
   }
